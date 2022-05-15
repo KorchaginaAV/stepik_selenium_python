372f4a7a42fdf150308630a7c2a931fd6762da85
Anastasiya <a1234@MacBook-Pro.local>
2022-05-15 16:49:50 +0400

    Отредактировала тест страницы логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        current_url = self.browser.current_url()
+        current_url = self.browser.current_url
         assert current_url.find("login") != -1, f'Текущий url "{current_url}", а не страница логина'
 
     def should_be_login_form(self):
